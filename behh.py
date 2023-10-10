2f7ba22bcbf0e3f92f27fca310ecfc4265103ed0
Yasmin Carla Gomes de Moura Oliveira <20231174010008@ifrn.local>
2023-10-10 16:47:12 -0300

    first commit

---
+++ behh.py
@@ -0,0 +1,10 @@
+import turtle
+leo = turtle.Turtle()
+for _ in range(12):
+    leo.forward(60)
+    leo.right(30)
+    leo.forward(60)
+    leo.right(150)
+    leo.forward(60)
+    leo.right(30)
+    leo.forward(60)
---
+++ boh.py
@@ -0,0 +1,5 @@
+import turtle
+leo = turtle.Turtle()
+for _ in range(6):
+    leo.forward(100)
+    leo.right(60)
